3d3f2d7c8731626c590fee344b28a29f048b46b4
Eliarlan Rodrigues <eliarlanrodrigues7@gmail.com>
2022-09-26 14:20:21 -0300

    Montando combo de forma dinamica

--- src/main/java/br/com/eliarlan/siscont/controller/TituloController.java
+++ src/main/java/br/com/eliarlan/siscont/controller/TituloController.java
@@ -1,12 +1,17 @@
 package br.com.eliarlan.siscont.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.eliarlan.siscont.model.StatusTitulo;
 import br.com.eliarlan.siscont.model.Titulo;
 import br.com.eliarlan.siscont.repository.TituloRepository;
 
@@ -28,6 +33,11 @@ public class TituloController {
         ModelAndView mv = new ModelAndView("titulo/formulario");
         mv.addObject("mensagem", "Título salvo com sucesso!");
         return mv;
+    }
+
+    @ModelAttribute("todosStatusTitulo")
+    public List<StatusTitulo> todosStatusTitulo() {
+        return Arrays.asList(StatusTitulo.values());
     }
 
 }
