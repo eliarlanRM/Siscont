1d77dbddd509a40931a313dfc3ab73bccc7f6c31
Eliarlan Rodrigues <eliarlanrodrigues7@gmail.com>
2022-09-26 11:29:24 -0300

    first commit

---
+++ src/main/java/br/com/eliarlan/siscont/SiscontApplication.java
@@ -0,0 +1,13 @@
+package br.com.eliarlan.siscont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eliarlan/siscont/SiscontApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eliarlan.siscont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiscontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
